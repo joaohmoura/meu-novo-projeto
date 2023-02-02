1a5b218c181a8a9f4d5cb26f5ca4a75791733a3e
Joao Henrique <j100fthigh@gmail.com>
2023-02-02 09:02:06 -0300

    commit inicial

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -28,3 +28,27 @@ let string = "Cristiano, Ronaldo, Alaba,Messi"
 let NumeroDeVogais = ContagemDeVogais(in: string)
 print(NumeroDeVogais)
 
+func countPalavras(text: String) -> Int {
+    let palavras = text.split(separator: " ")
+    return palavras.count
+}
+
+let texto = "Meu mentor é fera em Swift"
+let palavraContagem = countPalavras(text: texto)
+print("Número de palavras: \(palavraContagem)")
+
+func countConsoantes(in string: String) -> Int {
+    let consoantes = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+    var count = 0
+    for character in string {
+        if consoantes.contains(character) {
+            count += 1
+        }
+    }
+    return count
+}
+
+let exemplo = "Vamos pro Japão, Mestre Poli"
+let consoanteCount = countConsoantes(in: exemplo)
+print("O número de consoantes em '\(exemplo)' é \(consoanteCount)")
+
